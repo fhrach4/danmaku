9528eb5234558af21f69226d066f36c8f9915724
Frank Hrach <fjh3938@rit.edu>
2012-02-17 12:09:23 -0500

    Constants class updated

---
+++ majorProject/majorProject/constants.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace majorProject
+{
+    class Constants
+    {   
+        //#PLAYER CONSTANTS
+        //Player Movement Constants
+        public readonly int MAX_HUMAN_SPEED = 7;
+        public readonly int HUMAN_START_X = 300;
+        public readonly int HUMAN_START_Y = 500;
+
+        //Player Sprite Constants
+        public readonly int MIN_HUMAN_FRAMES = 0;
+        public readonly int MAX_HUMAN_FRAMES = 10;
+        public readonly int HUMAN_NEUTRAL_FRAME = 5;
+        public readonly int HUMAN_SPRITE_WIDTH = 34;
+        public readonly int HUMAN_SPRITE_HEIGHT = 38;
+
+        //#GRUNT CONSTANTS
+        //Grunt sprite constants
+        public readonly int GRUNT_SPRITE_WIDTH = 34;
+        public readonly int GRUNT_SPRITE_HEIGHT = 38;
+
+        public Constants()
+        {
+        }
+    }
+}
